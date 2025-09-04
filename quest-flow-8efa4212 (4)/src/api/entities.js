3ca7c470ae7ca9828547c29552ecd2ebe92383e2
sefunmi4 <sefunmiashiru0@gmail.com>
2025-09-04 14:12:59 -0400

    reset application to latest back up

---
+++ quest-flow-8efa4212 (4)/src/api/entities.js
@@ -0,0 +1,39 @@
+import { base44 } from './base44Client';
+
+
+export const Quest = base44.entities.Quest;
+
+export const Guild = base44.entities.Guild;
+
+export const QuestLog = base44.entities.QuestLog;
+
+export const HelpRequest = base44.entities.HelpRequest;
+
+export const QuestRating = base44.entities.QuestRating;
+
+export const TeamApplication = base44.entities.TeamApplication;
+
+export const UserSwipe = base44.entities.UserSwipe;
+
+export const Match = base44.entities.Match;
+
+export const QuestLike = base44.entities.QuestLike;
+
+export const QuestComment = base44.entities.QuestComment;
+
+export const GuildMembership = base44.entities.GuildMembership;
+
+export const UserFollow = base44.entities.UserFollow;
+
+export const Notification = base44.entities.Notification;
+
+export const BugReport = base44.entities.BugReport;
+
+export const Party = base44.entities.Party;
+
+export const PartyMessage = base44.entities.PartyMessage;
+
+
+
+// auth sdk:
+export const User = base44.auth;
